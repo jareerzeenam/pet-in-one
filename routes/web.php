ea1f26069085b6c071c75966db512b1a623cf147
jareerzeenam <jareerzeenam.28@gmail.com>
2020-06-15 01:16:14 +0530

    Application Completed & Ready To deploy

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+Route::get('/clear-cache', function() {
+    Artisan::call('cache:clear');
+    return view('welcome');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
